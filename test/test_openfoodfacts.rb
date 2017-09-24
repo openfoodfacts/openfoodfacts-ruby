0f3e4be899898d0826a1b149fc7eaf9ce606bb33
Nicolas Leger <nicolas.leger@nleger.com>
2017-09-25 00:32:15 +0200

    Add Missions

--- test/test_openfoodfacts.rb
+++ test/test_openfoodfacts.rb
@@ -182,6 +182,22 @@ class TestOpenfoodfacts < Minitest::Test
     end
   end
 
+  # Mission
+
+  def test_it_fetches_missions
+    VCR.use_cassette("missions") do
+      refute_empty ::Openfoodfacts::Mission.all(locale: 'fr')
+    end
+  end
+
+  def test_it_fetches_mission
+    VCR.use_cassette("mission", record: :once, match_requests_on: [:host, :path]) do
+      mission = ::Openfoodfacts::Mission.new(url: "https://fr.openfoodfacts.org/mission/informateur-100-produits")
+      mission.fetch
+      refute_empty mission.users
+    end
+  end
+
   # Press
 
   def test_it_fetches_press
