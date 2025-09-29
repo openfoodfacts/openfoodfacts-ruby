f5766744eff1781687929e5a5f238c69a97a4258
nicolasleger <nicolasleger@users.noreply.github.com>
2025-09-29 14:31:35 +0200

    feat: make wrapper generic with more paths

---
+++ lib/openfoodfacts/ingredient.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'hashie'
+
+module Openfoodfacts
+  class Ingredient < Hashie::Mash
+    # TODO: Add more locales
+    LOCALE_PATHS = {
+      'fr' => 'ingredients',
+      'uk' => 'ingredients',
+      'us' => 'ingredients',
+      'world' => 'ingredients'
+    }.freeze
+
+    class << self
+      # Get labels
+      #
+      def all(locale: DEFAULT_LOCALE, domain: DEFAULT_DOMAIN)
+        if (path = LOCALE_PATHS[locale])
+          Product.tags_from_page(self, "https://#{locale}.#{domain}/#{path}")
+        end
+      end
+    end
+
+    # Get products with label
+    #
+    def products(page: -1)
+      Product.from_website_page(url, page: page, products_count: products_count) if url
+    end
+  end
+end
